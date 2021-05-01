87c6ab09e3055a2f1983a50590072a4337a81d0e
danielsilvarj <daniel.cg.rio@gmail.com>
2021-04-30 21:47:42 -0300

    initial commit

---
+++ ProjetoMVC01.Domain/Entities/Fornecedor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoMVC01.Domain.Entities
+{
+   public class Fornecedor
+    {
+        public Guid IdFornecedor { get; set; }
+        public string Nome { get; set; }
+        public string CNPJ { get; set; }
+
+        #region Associações
+
+        public List<Produto> Produtos { get; set; }
+        
+        #endregion
+
+    }
+}
